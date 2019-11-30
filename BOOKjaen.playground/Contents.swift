8bf079f06c4be4eb18a047cde0a02686b480925e
Hailey <foxwavez@gmail.com>
2019-12-01 01:48:28 +0900

    복습

---
+++ BOOKjaen.playground/Contents.swift
@@ -0,0 +1,32 @@
+for row in 1...5 {
+    print(row)
+}
+for row in 1...9 {
+    print("2 X \(row) = \(row * 2)")
+}
+
+var lang = "swift"
+for char in lang {
+    print("개별 문자는 \(char)입니다.")
+}
+
+let size = 5
+let padChar = "0"
+var keyword = "3"
+
+for _ in 1...size {
+    keyword = padChar + keyword
+}
+print("\(keyword)")
+
+for i in 1..<10 {
+    for j in 1..<10 {
+        print("\(i) X \(j) = \( i * j)")
+    }
+}
+
+var n = 2
+while n < 1000 {
+    n = n * 2
+}
+print("n = \(n)")
