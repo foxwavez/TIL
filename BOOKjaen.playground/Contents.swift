29f456b6e2e3c7135b99127d062bef7b49b76bec
Hailey <foxwavez@gmail.com>
2019-12-02 17:40:36 +0900

    함수를 파라미터값으로 사용할 수 있는 지 테스트

--- BOOKjaen.playground/Contents.swift
+++ BOOKjaen.playground/Contents.swift
@@ -72,3 +72,131 @@ func divide(base: Int) {
 divide(base: 102)
 divide(base: 0)
 divide(base: 9)
+
+func getUseInfo() -> (Int, Character, String) {
+    let gender: Character = "M"
+    let height = 180
+    let name = "꼼꼼한 재은씨"
+    return (height, gender, name)
+}
+
+var uInfo = getUseInfo()
+uInfo.0
+
+var (a,b,c) = getUseInfo()
+
+func printHello(_ name: String, _ msg: String) {
+    print("\(name)님 , \(msg)")
+}
+printHello("홍길뿅", "안녕하세용")
+
+func avg(score:Int...) -> Double {
+    var total = 0
+    for r in score {
+        total = total + r
+    }
+    return (Double(total)/Double(score.count))
+}
+print(avg(score: 10, 20, 30, 40))
+
+// 기본값이 지정된 함수
+func echo(message: String, newLine: Bool = true){
+    if newLine == true {
+        print(message, true)
+    } else {
+        print(message, false)
+    }
+}
+echo(message: "안녕하세요")
+echo(message: "안녕하세요", newLine: true)
+echo(message: "안녕하세요", newLine: false)
+
+func incrementBy(base: Int) -> Int {
+    let base = base + 1
+    return base
+}
+incrementBy(base: 9)
+
+func descAge(name: String, _ paramAge: Int) -> String {
+    var name = name
+    var paramAge = paramAge
+    
+    // 입력된 값을 변경합니다.
+    name = name + "씨"
+    paramAge = paramAge + 1
+    return "\(name)의 내년 나이는 \(paramAge)세 입니다."
+}
+descAge(name: "희진", 24)
+ 
+var cnt = 30
+
+func autoIncrement(value: Int) -> Int {
+    var value = value
+    value = value + 1
+    return value
+}
+
+print(autoIncrement(value: cnt))
+print(cnt)
+
+var count = 30
+
+func foo(count: Int) -> Int {
+    var count = count
+    count = count + 1
+    return count
+}
+print(foo(count: count))
+print(count)
+
+func coo(base: Int) -> String {
+    print("함수 coo가 실행됩니다.")
+    return "결과값은 \(base + 1)입니다"
+}
+let fn1 = coo(base: 9)
+let fn2 = foo
+fn2(7)
+
+func boo(age: Int) -> String {
+    return "\(age)"
+}
+
+func boo(age: Int, name: String, height: Int) -> String {
+    return "\(name)의 나이는 \(age)세 입니다, 키는 \(height)"
+}
+
+let t:(Int, String, Int) -> String = boo
+let t1:(Int) -> String = boo(age:)
+let t2 = boo(age: 9)
+let t3 = boo(age: 19, name: "짤랑", height: 198)
+
+func boo(age: Int, name: String) -> String {
+    return "\(name)의 나이는 \(age)세 입니다."
+}
+
+func boo(height: Int, nick: String) -> String {
+    return "\(nick)의 키는 \(height)cm 입니다."
+}
+let fn03 = boo(age:name:)
+let fn04 = boo(height:nick:)
+
+func desc() -> String {
+    return "this is desc()"
+}
+
+func pass() -> () -> String { // 오른쪽 반환타아입은 함수 타입으로 작성되어 있음을 알 수 있다.
+    return desc
+}
+
+let p = pass()
+p()
+
+func incr(param: Int) -> Int {
+    return param + 1
+}
+
+func broker(base: Int, function fn: (Int)-> Int) -> Int {
+    return fn(base)
+}
+
+broker(base: 3, function: incr(param:))
