67dde151fbfc53d6340ed822e4ff2fc9c5827271
Hailey <foxwavez@gmail.com>
2019-12-05 00:26:46 +0900

    custom alert 버튼까지 UI 배치 완료

--- 19-12-03/customAlert/customAlert/SecondViewController.swift
+++ 19-12-03/customAlert/customAlert/SecondViewController.swift
@@ -28,6 +28,7 @@ class SecondViewController: UIViewController {
         
         let baseWidth = view.frame.width - 40
         let baseHeight = CGFloat(200)
+        let itemHeight = CGFloat(50)
         
         baseView.frame.size = CGSize(width: baseWidth, height: baseHeight)
         baseView.center = CGPoint(x: view.center.x, y: 300)
@@ -37,9 +38,42 @@ class SecondViewController: UIViewController {
         titleLabel.text = "title"
         titleLabel.textAlignment = .center
         titleLabel.textColor = .black
-        titleLabel.frame.size = CGSize(width: baseWidth, height: 50)
+        titleLabel.frame.size = CGSize(width: baseWidth, height: itemHeight)
         titleLabel.frame.origin = CGPoint(x: 0, y: 0)
         baseView.addSubview(titleLabel)
+        
+        messageLabel.text = "message"
+        messageLabel.textAlignment = .center
+        messageLabel.textColor = .black
+        messageLabel.frame = CGRect(x: 0, y: titleLabel.frame.maxY, width: baseWidth, height: itemHeight)
+        baseView.addSubview(messageLabel)
+        
+        textField.becomeFirstResponder()
+        textField.borderStyle = .roundedRect
+        textField.frame = CGRect(x: 20, y: messageLabel.frame.maxY, width: baseWidth - 40, height: itemHeight)
+        baseView.addSubview(textField)
+        
+        cancelButton.setTitle("취소", for: .normal)
+        cancelButton.frame = CGRect(
+            x: 0, y: textField.frame.maxY,
+            width: baseWidth / 2, height: itemHeight)
+        cancelButton.setTitleColor(.systemBlue, for: .normal)
+        cancelButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        baseView.addSubview(cancelButton)
+        
+        enterButton.setTitle("확인", for: .normal)
+        enterButton.frame = CGRect(
+            x: cancelButton.frame.maxX, y: textField.frame.maxY,
+            width: baseWidth / 2, height: itemHeight)
+        enterButton.setTitleColor(.systemBlue, for: .normal)
+        enterButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        baseView.addSubview(enterButton)
+    }
+    
+    @objc private func buttonAction(_ sender: UIButton) {
+        guard let firstVC = presentingViewController as? FirstViewController else { return }
+        firstVC.displayLabel.text = ""
+        dismiss(animated: false)
     }
 
 }
