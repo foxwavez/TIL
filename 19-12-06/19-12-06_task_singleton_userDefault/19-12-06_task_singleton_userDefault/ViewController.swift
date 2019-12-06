9c2b1492a9d5fe2ca3b126e824d3a84d4c78a54b
Hailey <foxwavez@gmail.com>
2019-12-06 16:04:12 +0900

    switch 누를 때마다 이미지와 라벨 데이터가 바뀌는 거 구현 완료

---
+++ 19-12-06/19-12-06_task_singleton_userDefault/19-12-06_task_singleton_userDefault/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  19-12-06_task_singleton_userDefault
+//
+//  Created by Hailey Lee on 2019/12/06.
+//  Copyright © 2019 Hailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var toggle: UISwitch!
+    
+    
+    var img = UIImage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toggle.addTarget(self, action: #selector(toggleTouched), for: .touchUpInside)
+        img = UIImage(named: "dog")!
+        imgView.image = img
+        label.text = "dog"
+        
+    }
+    @objc private func toggleTouched(_ sender: UISwitch) {
+        if toggle.isOn {
+            img = UIImage(named: "dog")!
+            imgView.image = img
+            label.text = "dog"
+        }
+        else {
+            img = UIImage(named: "cat")!
+            imgView.image = img
+            label.text = "cat"
+        }
+    }
+    // swithch에서 .setOn을 사용하면 애니메이션까지 해줄 수 있다
+    // .setOn(on: Bool, animated: Bool)이 이렇게 나오게 된다
+}
+
