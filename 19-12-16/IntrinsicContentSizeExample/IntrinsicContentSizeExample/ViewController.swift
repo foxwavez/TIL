137f6e3bb35c43c2a9c4e262f78a3bf7117c3c07
Hailey <foxwavez@gmail.com>
2019-12-16 14:55:58 +0900

    Intrinsic Content Size 스토리보드로 연습 구현

---
+++ 19-12-16/IntrinsicContentSizeExample/IntrinsicContentSizeExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntrinsicContentSizeExample
+//
+//  Created by Hailey Lee on 2019/12/16.
+//  Copyright © 2019 Hailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
