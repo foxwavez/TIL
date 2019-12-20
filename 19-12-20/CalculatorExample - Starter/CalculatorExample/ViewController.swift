c5fc5a5f2206e8c82e201117b512c43ed8f35e8a
Hailey <foxwavez@gmail.com>
2019-12-20 23:00:55 +0900

    계산기 예제 tag로 계산기능 구현까지 완료

--- 19-12-20/CalculatorExample - Starter/CalculatorExample/ViewController.swift
+++ 19-12-20/CalculatorExample - Starter/CalculatorExample/ViewController.swift
@@ -13,22 +13,84 @@ final class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     var numberOnScreen: Double = 0 //
-    var previousNumber: Double = 0  //
+    var previousNumber: Double = 0 //
     var operation = 0
+    var isPerformOperation = false
+    
     
     
   override func viewDidLoad() {
     super.viewDidLoad()
     resultLabel.text = ""
+    
   }
     
     @IBAction func numberAction(_ sender: UIButton) {
         let tag = sender.tag
-        resultLabel.text = (resultLabel.text ?? "") + String(tag - 1)
+                if isPerformOperation == true {
+                    isPerformOperation = false
+                    resultLabel.text = String(tag - 1)
+                    numberOnScreen = Double(resultLabel.text!)!
+                } else {
+                    resultLabel.text = resultLabel.text! + String(tag - 1)
+                    numberOnScreen = Double(resultLabel.text!)!
+                }
+        //        resultLabel.text = resultLabel.text! + String(tag - 1)
+
+
     }
     
-    
-    
-     
-}
+    @IBAction func operatorAction(_ sender: UIButton) {
+        
+        let tag = sender.tag
+        //  AC 버튼
+        if tag == 11 {
+            resultLabel.text = "0"
+            previousNumber = 0
+            numberOnScreen = 0
+            operation = 0
+        }
+        // +(16) -(15) *(14) /(13) =(12)기능
+        switch tag {
+        case 16, 15, 14, 13:
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            formatter.minimumFractionDigits = 0
+            formatter.maximumFractionDigits = 3
+            let numString = formatter.string(from: numberOnScreen as NSNumber)
+            print(numString!)
+            
+            previousNumber = Double(resultLabel.text ?? "")!
+            operation = tag
+            isPerformOperation = true
+            resultLabel.text = String(numString!)
+        case 12:
+            switch operation {
+            case 16:
+                let plusResult = previousNumber + numberOnScreen
+                
+                let formatter = NumberFormatter()
+                formatter.numberStyle = .decimal
+                formatter.minimumFractionDigits = 0
+                formatter.maximumFractionDigits = 3
+                let numString = formatter.string(from: plusResult as NSNumber)
+                print(numString!)
+                
+                resultLabel.text = numString!
+            case 15:
+                resultLabel.text = String(previousNumber - numberOnScreen)
+            case 14:
+                resultLabel.text = String(previousNumber * numberOnScreen)
+            case 13:
+                resultLabel.text = String(previousNumber / numberOnScreen)
+            default:
+                resultLabel.text = ""
+            }
+        default:
+            resultLabel.text = ""
+        }
+        
+    }
+    }
+
 
