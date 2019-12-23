301a3ce6a0635c87b2c23bd0ef9a3e1cd6617c0e
Hailey <foxwavez@gmail.com>
2019-12-24 02:37:30 +0900

    계산기 플러스 기능, 플러스 예외 처리 기능까지 구현 완료

--- 19-12-23/CalculatorExample - Starter2/CalculatorExample/ViewController.swift
+++ 19-12-23/CalculatorExample - Starter2/CalculatorExample/ViewController.swift
@@ -34,7 +34,6 @@ final class ViewController: UIViewController {
     @IBOutlet weak var equalBtn: UIButton!
     @IBOutlet weak var resetBtn: UIButton!
     
-    private var displayValue: String = ""
     
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -45,7 +44,6 @@ final class ViewController: UIViewController {
     private func setupUI() {
         for btn in buttons{
             btn.layer.cornerRadius = oneBtn.frame.height / 2 + 4
-//            btn.layer.cornerRadius = 45.5
         }
     }
     
@@ -53,26 +51,113 @@ final class ViewController: UIViewController {
         UIView.animate(withDuration: 0.2) {
             sender.alpha = 0.4
             sender.alpha = 1
-            
+        }
+    }
+    func formatter(number: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 3
+        guard let numString = formatter.string(from: number as NSNumber) else { return "" }
+        return numString
+    }
+    
+    private var displayText = ""
+    var displayValue: Double = 0
+    var beforeValue: Double = 0
+    var operatorValue: Double = 0
+    var isPerformOperation = false
+    var isPerformPlus = false
+    var isPerformMinus = false
+    
+    @IBAction func numberBtnAction(_ sender: UIButton) {
+        
+        guard let identifier = sender.accessibilityIdentifier else { return }
+        if isPerformOperation == true {
+            isPerformOperation = false
+            displayText = identifier
+            guard let number = Double(displayText) else { return }
+            displayLabel.text = formatter(number: number)
+            displayValue = number
+            print("character \(number)")
+        }else{
+            displayText = displayText + identifier
+            displayValue = Double(displayText)!
+            guard let number = Double(displayText) else { return }
+            displayLabel.text = formatter(number: number)
+            print("String \(displayText)")
         }
     }
     
-    @IBAction func numberBtnAction(_ sender: UIButton) {
-
-        guard let identifier = sender.accessibilityIdentifier else { return }
-        guard let number = Double(identifier) else { return }
-        
-        
-        print(number)
-        displayLabel.text = "\(number)"
-    }
-    
     @IBAction func operatorBtnAction(_ sender: UIButton) {
-    
-    
+        guard let identifier = sender.accessibilityIdentifier else { return }
+        isPerformOperation = true
+        switch identifier {
+        case "plus":
+            print("plus - \(Double(displayText)!), \(displayValue)")
+            beforeValue = displayValue
+            isPerformPlus = true
+        case "minus":
+            print("plus - \(Double(displayText)!), \(displayValue)")
+            beforeValue = displayValue
+            isPerformMinus = true
+        default:
+            break
+        }
+        
+//        switch identifier {
+//        case "equal":
+//            let plusValue = Double(displayText)! + beforeValue
+//            print("equal")
+//            operatorValue = plusValue
+//            displayLabel.text = formatter(number: plusValue)
+//        default:
+//            break
+//        }
+        
     }
+    
+    @IBAction func equalBtnAction(_ sender: UIButton) {
+        guard let identifier = sender.accessibilityIdentifier else { return }
+        if identifier == "equal" {
+            if isPerformPlus == true {
+                let plusValue = beforeValue + Double(displayText)!
+                print("equal - \(beforeValue), \(Double(displayText)!)")
+                operatorValue = plusValue
+                displayLabel.text = formatter(number: plusValue)
+                isPerformPlus = false
+            } else if isPerformPlus == false {
+                operatorValue = operatorValue + Double(displayText)!
+                displayLabel.text = formatter(number: operatorValue)
+                isPerformPlus = false
+            }
+//            if isPerformMinus == true {
+//                let minusValue = beforeValue - Double(displayText)!
+//                print("equal - \(beforeValue), \(Double(displayText)!)")
+//                operatorValue = minusValue
+//                displayLabel.text = formatter(number: minusValue)
+//                isPerformMinus = false
+//            } else if isPerformMinus == false {
+//                operatorValue = operatorValue - Double(displayText)!
+//                displayLabel.text = formatter(number: operatorValue)
+//                isPerformMinus = false
+//            }
+        }
+    }
+    
     
     @IBAction func resetBtnAction(_ sender: UIButton) {
+        guard let identifier = sender.accessibilityIdentifier else { return }
+        if identifier == "reset"{
+            displayValue = 0
+            operatorValue = 0
+            beforeValue = 0
+            displayText = "0"
+            displayLabel.text = "0"
+        }
+        else {
+            
+        }
     }
     
 }
