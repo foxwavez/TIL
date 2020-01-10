c65d5832c4230a6dce59089863cb25ceebed0bad
Hailey <foxwavez@gmail.com>
2020-01-10 18:16:03 +0900

    스크롤뷰 페이지 컨트롤 설정 코드로 구현 완료 주석까지 작성

--- 20-01-10/ScrollViewExample/ScrollViewExample/ViewController.swift
+++ 20-01-10/ScrollViewExample/ScrollViewExample/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-   
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControll: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.delegate = self
+        setPageControl()
     }
   
+    private func setPageControl() {
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width * 2, height: UIScreen.main.bounds.height)
+        // UIScreen  -> 해당 디바이스의 스크린 전체 크기
+        
+        pageControll.isUserInteractionEnabled = false
+        // isUserInterationEnabled: 사용자 이벤트가 무시되고 이벤트 큐에서 제거되는지 여부를 결정하는 부울 값입니다.
+        // pageControll의 이벤트를 무시하지 말라고 false 값을 주었다
+    }
+}
 
+// MARK: - UIScrollViewDelegate
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControll.currentPage = Int(floor(scrollView.contentOffset.x / UIScreen.main.bounds.width))
+        // contentOffset.x는 모든 페이지가 합쳐진 width 길이
+        // UIScreen.main.boinds.width는 디바이스 화면에 나오는 뷰
+        // pageControll은 width가 어디서부터 어디까지 현재 화면을 뜻하는지 알지 못한다
+        // 그래서 알려주기 위해서 contentOffset.x / UIScreen.main.boinds.width
+        // 나눠서 현재 페이지를 알려준다
+        // currentPage 현재 페이지가 어느 시점부터
+        
+    }
 }
 
--- FC:reference/25. ScrollView/ScrollViewExample - Starter/ScrollViewExample/ViewController.swift
+++ FC:reference/25. ScrollView/ScrollViewExample - Starter/ScrollViewExample/ViewController.swift
@@ -15,25 +15,76 @@ final class ViewController: UIViewController {
   
   @IBOutlet private weak var scrollView: UIScrollView!
   @IBOutlet private weak var imageView: UIImageView!
+    
+    private var zoomScale: CGFloat = 1.0 {
+        didSet {
+            print("Current Zoom Scle:", String(format: "%.2f", zoomScale))
+        }
+    }
+   
   
   // MARK: LifeCycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    scrollView.delegate = self
+//    scrollView.bounds = true
+    // scrollView에 딱 맞는 사이즈가 되어도 스크롤 가능,bounds 효과 가로 세로 별도
+    scrollView.alwaysBounceHorizontal = true
+    scrollView.alwaysBounceVertical = true
+    updateScrollViewScale()
   }
+    
+    private func updateScrollViewScale() {
+        let widthScale = view.frame.width / imageView.bounds.width
+        let heightScale = view.frame.height / imageView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.zoomScale = 1
+        scrollView.maximumZoomScale = 3
+    }
   
   // MARK: Action Handler
   
   @IBAction private func fitToWidth(_ sender: Any) {
     print("\n---------- [ fitToWidth ] ----------")
+    zoomScale = scrollView.frame.width / imageView.bounds.width
+    scrollView.setZoomScale(zoomScale, animated: true)
+    print(zoomScale)
+    
   }
   
   @IBAction private func scaleDouble(_ sender: Any) {
     print("\n---------- [ scaleDouble ] ----------")
+    scrollView.setZoomScale(zoomScale * 2, animated: true)
+    zoomScale = scrollView.zoomScale
+    // 실제 결과 최종 scrollView의 zoomScale 값이 다르다
+    // scrollView의 zoomScale은 maximum/minimuZoomSclae에서 정한 값을 벗어나지 않는다
+    
+//    let rectToVisible = CGRect(x: 100, y: 100, width: 200, height: 300)
+//    scrollView.zoom(to: rectToVisible, animated: true)
+    // zoom 이 배율이 얼만큼 될건지는 위에 있는 코드가 정해준다
+    print(zoomScale)
+    
   }
 
   @IBAction private func moveContentToLeft(_ sender: Any) {
     print("\n---------- [ moveContentToLeft ] ----------")
+    let newOffset = CGPoint(x: scrollView.contentOffset.x + 150, y: scrollView.contentOffset.y)
+    scrollView.setContentOffset(newOffset, animated: true)
   }
 }
+ 
+// MARK: - UIScrollViewDelegate
 
+extension ViewController: UIScrollViewDelegate {
+    // 뭐를 줄이는지 뷰를 통해서 설정을 해야 한다
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // 스크롤로 움직일 때마다 호출 되는 메서드
+        // 프린트를 넣으면 움직일 때마다 프린트가 찍힌다
+    }
+}
